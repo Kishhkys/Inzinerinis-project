5eedb7a528a2bd38b037ebc7dec337fc28c23dc3
DevMantelis <m.uscilajoo@gmail.com>
2026-03-08 13:21:25 +0200

    Add player sprite, movement animations and camera follow

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,23 +3,31 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private float moveSpeed = 3.0f;
 
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
     {
         playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
     }
 
     private void Update()
@@ -30,6 +38,24 @@ public class PlayerController : MonoBehaviour
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        bool isMoving = movement.sqrMagnitude > 0;
+        animator.SetBool("isMoving", isMoving);
+
+        if (isMoving)
+        {
+            if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+            {
+                animator.SetFloat("horizontal", movement.x);
+                animator.SetFloat("vertical", 0f);
+            }
+            else
+            {
+                animator.SetFloat("horizontal", 0f);
+                animator.SetFloat("vertical", movement.y);
+            }
+        }
+
     }
 
     private void FixedUpdate()
@@ -39,6 +65,6 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * movement);
     }
 }
