f2e7be151380dc84b1844a7a9f460a3ec06d78a3
Karina Slapkauskaite <karinaslap@gmail.com>
2026-05-11 12:14:10 +0300

    Fixed collisions

--- Assets/Scripts/FileItem.cs
+++ Assets/Scripts/FileItem.cs
@@ -2,18 +2,36 @@ using UnityEngine;
 
 public class FileItem : Item
 {
-    [TextArea]
+    [TextArea(5, 15)]
     public string fileContent;
+
+    private PatientFilePopup patientFilePopup;
+
+    private void Start()
+    {
+        patientFilePopup = FindFirstObjectByType<PatientFilePopup>();
+    }
 
     public override void UseItem()
     {
         Debug.Log("Opening patient file...");
         Debug.Log(fileContent);
+
+        if (patientFilePopup != null)
+        {
+            patientFilePopup.ShowFile(fileContent);
+        }
+        else
+        {
+            Debug.LogWarning("PatientFilePopup not found in scene.");
+        }
     }
 
     public override void PickUp(string group = "File", string sound = "File_pick_up")
     {
         base.PickUp(group, sound);
         Debug.Log("Patient file collected");
+
+        UseItem();
     }
 }
--- Assets/Scripts/PatientFilePopup.cs
+++ Assets/Scripts/PatientFilePopup.cs
@@ -1,6 +1,36 @@
 using UnityEngine;
+using TMPro;
 
-public class PatientFilePopup
+public class PatientFilePopup : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject popupPanel;
+    [SerializeField] private TMP_Text contentText;
+
+    private void Start()
+    {
+        popupPanel.SetActive(false);
+    }
+
+    public void ShowFile(string content)
+    {
+        popupPanel.SetActive(true);
+        contentText.text = content;
+
+        Time.timeScale = 0f;
+    }
+
+    public void HideFile()
+    {
+        popupPanel.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+
+    private void Update()
+    {
+        if (popupPanel.activeSelf && Input.GetKeyDown(KeyCode.E))
+        {
+            HideFile();
+        }
+    }
 }
